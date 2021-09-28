914027849f1870c7afb9ad679316bc9284dceedc
minimadon <90094327+minimadon@users.noreply.github.com>
2021-09-28 16:41:53 +0900

    main.jsにViewModel/Project/Taskクラスを追加

--- todo/test/js/viewmodel.js
+++ todo/test/js/viewmodel.js
@@ -24,28 +24,29 @@
     }
 
     it('test project', function(){
-        let project = new Project(1, "テストProject");
+        let project = new Project(1, 'テストProject', '#777');
         assert(project.id === 1);
-        assert(project.name === "テストProject");
+        assert(project.name === 'テストProject');
+        assert(project.color === '#777');
     });
 
     it('test viewmodel projects', function(){
         let viewModel = new ViewModel();
         assert(viewModel.projects.length === 0);
 
-        let project = new Project(1, "テストProject");
-        viewModel.addProject(project);
+        let project = new Project(1, 'テストProject', '#777');
+        viewModel.projects.push(project);
         assert(viewModel.projects.length === 1);
 
-        viewModel.clearProjects();
+        viewModel.projects.splice(0);
         assert(viewModel.projects.length === 0);
     });
 
     it('test task', function(){
-        let task = new Task(1, 2, "テストTask", "2021-09-22", 3);
+        let task = new Task(1, 2, 'テストTask', '2021-09-22', 3);
         assert(task.id === 1);
         assert(task.projectId === 2);
-        assert(task.value === "テストTask");
+        assert(task.value === 'テストTask');
         assert(task.completetionDate === '2021-09-22');
         assert(task.status === 3);
     });
@@ -54,16 +55,23 @@
         let viewModel = new ViewModel();
         assert(viewModel.tasks.length === 0);
 
-        let task = new Task(1, 2, "テストTask", "2021-09-22", 3);
-        viewModel.addTask(task);
+        let task = new Task(1, 2, 'テストTask', '2021-09-22', 1);
+        viewModel.tasks.push(task);
         assert(viewModel.tasks.length === 1);
+        assert(viewModel.incompleteTasks.length === 1);
+        assert(viewModel.completedTasks.length === 0);
 
-        viewModel.clearTasks();
+        viewModel.tasks[0].status = 0;
+        assert(viewModel.tasks.length === 1);
+        assert(viewModel.incompleteTasks.length === 0);
+        assert(viewModel.completedTasks.length === 1);
+
+        viewModel.tasks.splice(0);
         assert(viewModel.tasks.length === 0);
     });
 
     it('test task completed', function(){
-        let task = new Task(1, 2, "テストTask", "2021-09-22", 1);
+        let task = new Task(1, 2, 'テストTask', '2021-09-22', 1);
         assert(task.isCompleted === false);
 
         task.status = 0;
@@ -75,11 +83,16 @@
         let yesterday = new Date();
         yesterday.setDate(yesterday.getDate() - 1);
 
-        let task = new Task(1, 2, "テストTask", `${today.getFullYear()}-${("0"+(today.getMonth()+1)).slice(-2)}-${("0"+today.getDate()).slice(-2)}`, 1);
+        let task = new Task(1, 2, 'テストTask', `${today.getFullYear()}-${('0'+(today.getMonth()+1)).slice(-2)}-${('0'+today.getDate()).slice(-2)}`, 1);
         assert(task.isCompleted === false);
         assert(task.isExpired === false);
 
-        task.completetionDate = `${yesterday.getFullYear()}-${("0"+(yesterday.getMonth()+1)).slice(-2)}-${("0"+yesterday.getDate()).slice(-2)}`;
+        task.completetionDate = `${yesterday.getFullYear()}-${('0'+(yesterday.getMonth()+1)).slice(-2)}-${('0'+yesterday.getDate()).slice(-2)}`;
+        assert(task.isCompleted === false);
         assert(task.isExpired === true);
+
+        task.status = 0;
+        assert(task.isCompleted === true);
+        assert(task.isExpired === false);
     });
 })();
