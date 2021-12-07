534a1ec689d2a23b3c72ec6cffe9e4a1ad09190b
minimadon <90094327+minimadon@users.noreply.github.com>
2021-12-07 14:03:56 +0900

    main.js ViewModelクラス リファクタリング

--- todo/srcFolder/index.php
+++ todo/srcFolder/index.php
@@ -12,67 +12,82 @@
     <title>To-Do</title>
 </head>
 
-<body data-bind="style: { width: isMobile() ? '100%' : '800px', margin: isMobile() ? '0' : '8px' }">
+<body data-bind="style: {
+    width: env.isMobile() ? '100%' : '800px',
+    margin: env.isMobile() ? '0' : '8px' }">
     <div class="bodyDiv" data-bind="
         style: {
-            backgroundImage: backgroundImageHash() !== null ? `url(./post.php?todo=background-image&hash=${backgroundImageHash()})` : 'unset',
-            'font-family': selectedFontFamily() }">
+            backgroundImage: userSetting.backgroundImageHash() !== null ? `url(./post.php?todo=background-image&hash=${userSetting.backgroundImageHash()})` : 'unset',
+            'font-family': userSetting.selectedFontFamily() }">
         <div class="header">
             <i class="material-icons" data-bind="
-                click: isMobile() ? function(){displayColumn(!displayColumn());return true;} : function(){return true},
-                css: isMobile() ? 'hamburger' : 'icon-person',
-                text: isMobile() ? 'menu' : 'person'"></i>
+                click: env.isMobile() ? function(){column.display(!column.display());return true;} : function(){return true},
+                css: env.isMobile() ? 'hamburger' : 'icon-person',
+                text: env.isMobile() ? 'menu' : 'person'"></i>
             <p class="user">みんまるたすく ver1.7.0</p>
             <i class="icon-settings material-icons" data-bind="
-                click: function(){displaySettings(!displaySettings())},
-                visible: displaySettingsIcon">settings</i>
+                click: function(){userSetting.display(!userSetting.display())},
+                visible: userSetting.displayIcon">settings</i>
             <i id="sound" class="icon-settings material-icons">notifications_off</i>
         </div>
-        <div class="settings" data-bind="visible: displaySettings">
-            <i class="material-icons settings-icons" data-bind="click: function(){displaySettings(!displaySettings())}">clear</i>
+        <div class="settings" data-bind="visible: userSetting.display">
+            <i class="material-icons settings-icons" data-bind="click: function(){userSetting.display(!userSetting.display())}">clear</i>
             フォント:
             <select data-bind="
                 event: {
                     change: changeFontFamily
                 },
-                options: availableFontFamilies,
+                options: userSetting.availableFontFamilies,
                 optionsText: 'name',
                 optionsValue: 'fontFamily',
-                value: selectedFontFamily"></select>
+                value: userSetting.selectedFontFamily"></select>
             &nbsp;|&nbsp;
             <label>
                 背景画像:
                 <i class="material-icons settings-icons">image</i>
-                <span data-bind="text: `MAX:${imageFileMaxSize()}B`"></span>
+                <span data-bind="text: `MAX:${userSetting.imageFileMaxSize()}B`"></span>
                 <input type="file" class="settings-background-image-file" accept="image/*" data-bind="event: { change: changeBackgroundImage }"/>
             </label>
             <i class="material-icons settings-icons" data-bind="click: function(){updateBackgroundImage(null)}">delete</i>
         </div>
-        <div class="main" data-bind="style: { height: isMobile() ? 'auto' : '450px'}">
-            <div class="main-column" data-bind="visible: displayColumn">
-                <div data-bind="foreach: columns">
-                    <div class="column_box" data-bind="click: displayTasks, css: { selected_column: $root.selectedColumn() === $data }">
+        <div class="main" data-bind="style: { height: env.isMobile() ? 'auto' : '450px'}">
+            <div class="main-column" data-bind="visible: column.display">
+                <div data-bind="foreach: column.columns">
+                    <div class="column_box" data-bind="
+                        click: displayTasks,
+                        css: { selected_column: $root.column.selectedColumn() === $data }">
                         <i class="material-icons icon-column" data-bind="text: icon, style: { color: color }"></i>
                         <p data-bind="text: name"></p>
                         <p class="counts" data-bind="text: countIncompleteTasks"></p>
                     </div>
                 </div>
                 <hr class="hr_gray">
-                <div id="projects" class="main-column-projects" data-bind="foreach: projects">
-                    <div class="main-column-projects-project" data-bind="click: displayTasks, css: { selected_column: $root.selectedColumn() === $data }">
+                <div id="projects" class="main-column-projects" data-bind="foreach: column.projects">
+                    <div class="main-column-projects-project" data-bind="
+                        click: displayTasks,
+                        css: { selected_column: $root.column.selectedColumn() === $data }">
                         <label class="label_project" data-bind="attr: { 'for': `cp-project_id_${id}` }">
                             <i class="material-icons darkmode-ignore" data-bind="style: { color: color }, text: icon"></i>
                         </label>
                         <input type="color" class="color-picker" data-bind="
                             css: {
-                                'color-picker' : !$root.isMobile(),
-                                'color-picker-mobile' : $root.isMobile()
+                                'color-picker' : !$root.env.isMobile(),
+                                'color-picker-mobile' : $root.env.isMobile()
                             },
                             value: color,
                             attr: { 'id': `cp-project_id_${id}` },
                             event: { change: changeProjectColor }"/>
-                        <p class="name" data-bind="text: name, click: clickProjectName, hidden: $root.renameProject() === $data"></p>
-                        <input type="text" class="rename" maxlength="30" data-bind="visible: $root.renameProject() === $data, hasfocus: $root.renameProject() === $data, event: { keydown: keydownRenameProject, blur: blurProjectName }" placeholder="入力してEnter"/>
+                        <p class="name" data-bind="
+                            text: name,
+                            click: clickProjectName,
+                            hidden: $root.column.renameProject() === $data"></p>
+                        <input type="text" class="rename" maxlength="30" data-bind="
+                            visible: $root.column.renameProject() === $data,
+                            hasfocus: $root.column.renameProject() === $data,
+                            event: {
+                                keydown: keydownRenameProject,
+                                blur: blurProjectName
+                            }" placeholder="入力してEnter"/>
                         <i class="material-icons delete_project_button" data-bind="click: deleteProject, clickBubble: false">delete_forever</i>
                     </div>
                 </div>
@@ -85,55 +100,83 @@
             <div class="main-todo">
                 <div id="todo_title">
                     <label data-bind="
-                        click: selectedColumn() instanceof Project ? function(){return true;} : function(){return false;}">
+                        click: column.selectedColumn() instanceof Project ? function(){return true;} : function(){return false;}">
                         <input type="color" class="color-picker" data-bind="
                             css: {
-                                'color-picker' : !isMobile(),
-                                'color-picker-mobile' : isMobile()
+                                'color-picker' : !env.isMobile(),
+                                'color-picker-mobile' : env.isMobile()
                             },
-                            value: selectedColumn() !== null ? selectedColumn().color : '',
+                            value: column.selectedColumn() !== null ? column.selectedColumn().color : '',
                             event: { change: changeProjectColor }"/>
                         <i class="material-icons darkmode-ignore icon-column" data-bind="
-                            style: { color: selectedColumn() !== null ? selectedColumn().color : 'black' },
-                            text: selectedColumn() !== null ? selectedColumn().icon : ''"></i></label><span data-bind="
-                            text: selectedColumn() !== null ? selectedColumn().name : 'プロジェクト未選択'"/>
+                            style: { color: column.selectedColumn() !== null ? column.selectedColumn().color : 'black' },
+                            text: column.selectedColumn() !== null ? column.selectedColumn().icon : ''"></i></label><span data-bind="
+                            text: column.selectedColumn() !== null ? column.selectedColumn().name : 'プロジェクト未選択'"/>
                 </div>
-                <div class="main-todo-result" data-bind="style: { marginLeft: isMobile() ? '0px' : '30px', width: isMobile() ? '100%' : '500px'}">
-                    <div class="main-todo-result-incomplete" data-bind="style: { marginLeft: isMobile() ? '20%' : '130px'}">
-                        <p id="incomplete_task_count" data-bind="text: incompleteTasks().length"></p>
+                <div class="main-todo-result" data-bind="
+                    style: {
+                        marginLeft: env.isMobile() ? '0px' : '30px',
+                        width: env.isMobile() ? '100%' : '500px'}">
+                    <div class="main-todo-result-incomplete" data-bind="style: { marginLeft: env.isMobile() ? '20%' : '130px'}">
+                        <p id="incomplete_task_count" data-bind="text: task.incompleteTasks().length"></p>
                         <p>未完了</p>
                     </div>
-                    <div class="main-todo-result-complete" data-bind="style: { marginLeft: isMobile() ? '20%' : '130px'}">
-                        <p id="complete_task_count" data-bind="text: completedTasks().length"></p>
+                    <div class="main-todo-result-complete" data-bind="style: { marginLeft: env.isMobile() ? '20%' : '130px'}">
+                        <p id="complete_task_count" data-bind="text: task.completedTasks().length"></p>
                         <p>完了済み</p>
                     </div>
                 </div>
                 <div class="main-todo-body">
                     <div class="main-todo-body-tasks">
-                        <div class="main-todo-body-tasks-task" data-bind="visible: $root.selectedColumn() instanceof Project, style: { marginLeft: isMobile() ? '0px' : '30px', width: isMobile() ? '100%' : '500px' }">
+                        <div class="main-todo-body-tasks-task" data-bind="
+                            visible: column.selectedColumn() instanceof Project,
+                            style: {
+                                marginLeft: env.isMobile() ? '0px' : '30px',
+                                width: env.isMobile() ? '100%' : '500px' }">
                             <i class="material-icons">add</i>
-                            <input id="input_task" class="main-todo-body-tasks-task-text" type="text" maxlength="30" placeholder="ここにタスクを入力してEnter" data-bind="style: { width: isMobile() ? '50%' : '300px' }">
-                            <input id="input_date" class="main-todo-body-tasks-task-date" type="date" data-bind="style: { width: isMobile() ? '20%' : '140px' }">
+                            <input id="input_task" class="main-todo-body-tasks-task-text" type="text" maxlength="30" placeholder="ここにタスクを入力してEnter" data-bind="style: { width: env.isMobile() ? '50%' : '300px' }">
+                            <input id="input_date" class="main-todo-body-tasks-task-date" type="date" data-bind="style: { width: env.isMobile() ? '20%' : '140px' }">
                         </div>
                     </div>
-                    <div id="incomplete_tasks" data-bind="foreach: incompleteTasks">
-                        <div class="main-todo-body_tasks main-todo-body-incomplete_tasks" data-bind="style: { marginLeft: $root.isMobile() ? '0px' : '30px', width: $root.isMobile() ? '100%' : '500px'}">
+                    <div id="incomplete_tasks" data-bind="foreach: task.incompleteTasks">
+                        <div class="main-todo-body_tasks main-todo-body-incomplete_tasks" data-bind="
+                            style: {
+                                marginLeft: $root.env.isMobile() ? '0px' : '30px',
+                                width: $root.env.isMobile() ? '100%' : '500px'}">
                             <div class="task_project_color darkmode-ignore" data-bind="style: { 'background-color': project.color }">&nbsp;</div>
-                            <i class="material-icons check_task check-incomplete_task" data-bind="click: clickTaskStatus, hidden: $root.selectedColumn() === $root.columns[COLUMN_TYPE.COMPLETED]">crop_square</i>
-                            <p class="task_value task_value_incomplete" data-bind="text: value, style: { width: $root.isMobile() ? '55%' : '280px'}"></p>
-                            <p class="task_date task_date_incomplete" data-bind="text: $root.isMobile() ? completetionDate : `期限:${completetionDate}`, css: { red: isExpired }"></p>
+                            <i class="material-icons check_task check-incomplete_task" data-bind="
+                                click: clickTaskStatus,
+                                hidden: $root.column.selectedColumn() === $root.column.columns[COLUMN_TYPE.COMPLETED]">crop_square</i>
+                            <p class="task_value task_value_incomplete" data-bind="
+                                text: value,
+                                style: { width: $root.env.isMobile() ? '55%' : '280px'}"></p>
+                            <p class="task_date task_date_incomplete" data-bind="
+                                text: $root.env.isMobile() ? completetionDate : `期限:${completetionDate}`,
+                                css: { red: isExpired }"></p>
                             <i class="material-icons delete_task delete_incomplete_task" data-bind="click: clickTaskDelete">delete_forever</i>
                         </div>
                     </div>
                     <div id="complete_task_button" class="main-todo-body-complete_task_button" data-bind="click: openComplete">
-                        <p data-bind="text: $root.openCompleteTaskFlag() == 0 ? '完了済みのタスクを表示' : '完了済みのタスクを非表示'">完了済みのタスクを表示</p>
+                        <p data-bind="text: task.openCompleteTaskFlag() == 0 ? '完了済みのタスクを表示' : '完了済みのタスクを非表示'">完了済みのタスクを表示</p>
                     </div>
-                    <div id="complete_tasks" data-bind="foreach: completedTasks">
-                        <div class="main-todo-body_tasks main-todo-body-complete_tasks" data-bind="css: { hidden: $root.openCompleteTaskFlag() == 0 }, style: { marginLeft: $root.isMobile() ? '0px' : '30px', width: $root.isMobile() ? '100%' : '500px'}">
+                    <div id="complete_tasks" data-bind="foreach: task.completedTasks">
+                        <div class="main-todo-body_tasks main-todo-body-complete_tasks" data-bind="
+                            css: {
+                                hidden: $root.task.openCompleteTaskFlag() == 0
+                            },
+                            style: {
+                                marginLeft: $root.env.isMobile() ? '0px' : '30px',
+                                width: $root.env.isMobile() ? '100%' : '500px'}">
                             <div class="task_project_color darkmode-ignore" data-bind="style: { 'background-color': project.color }">&nbsp;</div>
-                            <i class="material-icons check_task check-complete_task" data-bind="click: clickTaskStatus, hidden: $root.selectedColumn() === $root.columns[COLUMN_TYPE.COMPLETED]">done</i>
-                            <p class="task_value task_value_complete" data-bind="text: value, style: { width: $root.isMobile() ? '55%' : '280px'}"></p>
-                            <p class="task_date task_date_complete" data-bind="text: $root.isMobile() ? completetionDate : `期限:${completetionDate}`, css: { red: isExpired }"></p>
+                            <i class="material-icons check_task check-complete_task" data-bind="
+                                click: clickTaskStatus,
+                                hidden: $root.column.selectedColumn() === $root.column.columns[COLUMN_TYPE.COMPLETED]">done</i>
+                            <p class="task_value task_value_complete" data-bind="
+                                text: value,
+                                style: { width: $root.env.isMobile() ? '55%' : '280px'}"></p>
+                            <p class="task_date task_date_complete" data-bind="
+                                text: $root.env.isMobile() ? completetionDate : `期限:${completetionDate}`,
+                                css: { red: isExpired }"></p>
                             <i class="material-icons delete_task delete_complete_task" data-bind="click: clickTaskDelete">delete_forever</i>
                         </div>
                     </div>
--- todo/test/js/viewmodel.js
+++ todo/test/js/viewmodel.js
@@ -26,20 +26,20 @@
     it('test project', function(){
         const project = new Project(1, 'テストProject', '#777777');
         assert(project.id === 1);
-        assert(project.name === 'テストProject');
+        assert(project.name() === 'テストProject');
         assert(project.color() === '#777777');
     });
 
     it('test viewmodel projects', function(){
         const viewModel = new ViewModel();
-        assert(viewModel.projects.length === 0);
+        assert(viewModel.column.projects.length === 0);
 
         const project = new Project(1, 'テストProject', '#777777');
-        viewModel.projects.push(project);
-        assert(viewModel.projects().length === 1);
+        viewModel.column.projects.push(project);
+        assert(viewModel.column.projects().length === 1);
 
-        viewModel.projects.removeAll();
-        assert(viewModel.projects().length === 0);
+        viewModel.column.projects.removeAll();
+        assert(viewModel.column.projects().length === 0);
     });
 
     it('test task', function(){
@@ -47,7 +47,7 @@
         const task = new Task(1, project, 'テストTask', '2021-09-22', 3);
         assert(task.id === 1);
         assert(task.project.id === 2);
-        assert(task.project.name === 'テストProject');
+        assert(task.project.name() === 'テストProject');
         assert(task.project.color() === '#777777');
         assert(task.value === 'テストTask');
         assert(task.completetionDate === '2021-09-22');
@@ -56,25 +56,25 @@
 
     it('test viewmodel tasks', function(){
         const viewModel = new ViewModel();
-        assert(viewModel.incompleteTasks().length === 0);
-        assert(viewModel.completedTasks().length === 0);
+        assert(viewModel.task.incompleteTasks().length === 0);
+        assert(viewModel.task.completedTasks().length === 0);
 
         const project = new Project(2, 'テストProject', '#777777');
         const task = new Task(1, project, 'テストTask', '2021-09-22', 1);
-        viewModel.incompleteTasks.push(task);
-        assert(viewModel.incompleteTasks().length === 1);
-        assert(viewModel.completedTasks().length === 0);
+        viewModel.task.incompleteTasks.push(task);
+        assert(viewModel.task.incompleteTasks().length === 1);
+        assert(viewModel.task.completedTasks().length === 0);
 
         task.status = 0;
-        viewModel.incompleteTasks.remove(task);
-        viewModel.completedTasks.push(task);
-        assert(viewModel.incompleteTasks().length === 0);
-        assert(viewModel.completedTasks().length === 1);
+        viewModel.task.incompleteTasks.remove(task);
+        viewModel.task.completedTasks.push(task);
+        assert(viewModel.task.incompleteTasks().length === 0);
+        assert(viewModel.task.completedTasks().length === 1);
 
-        viewModel.incompleteTasks.removeAll();
-        viewModel.completedTasks.removeAll();
-        assert(viewModel.incompleteTasks().length === 0);
-        assert(viewModel.completedTasks().length === 0);
+        viewModel.task.incompleteTasks.removeAll();
+        viewModel.task.completedTasks.removeAll();
+        assert(viewModel.task.incompleteTasks().length === 0);
+        assert(viewModel.task.completedTasks().length === 0);
     });
 
     it('test task completed', function(){
